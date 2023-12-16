90d4a3df8a2dfe9080b4e3dfee0e8194dfc0b1a1
Javier <javierpadin661@gmail.com>
2023-12-16 11:15:05 -0500

    integracion culqui

--- Views/Carrito/procesarpago.php
+++ Views/Carrito/procesarpago.php
@@ -240,6 +240,81 @@ $infoTerminos = !empty(getInfoPage(PTERMINOS)) ? getInfoPage(PTERMINOS)['conteni
 										<img src="<?= media()?>/images/img-paypal.jpg" alt="Icono de PayPal" class="ml-space-sm" width="74" height="20">
 									</label>
 								</div>
+<!-- culquiiiiiiiiiiiii -->
+								<p>hola culqui</p>
+								<script src="https://checkout.culqi.com/js/v4"></script>
+								
+								<button type="button" class="btn btn-default" id="btn_pagar_culqui">Pagar culqui</button>
+								<script>
+									Culqi.publicKey = 'pk_test_80012ab94f994cdf';
+
+									const btn_pagar = document.getElementById('btn_pagar_culqui');
+									btn_pagar.addEventListener('click', function (e) {
+									// Abre el formulario con la configuración en Culqi.settings y CulqiOptions
+										Culqi.settings({
+											title: 'Culqi Store',
+											currency: 'PEN',  // Este parámetro es requerido para realizar pagos yape
+											amount: 1000,  // Este parámetro es requerido para realizar pagos yape
+											order: 'ord_live_0CjjdWhFpEAZlxlz', // Este parámetro es requerido para realizar pagos con pagoEfectivo, billeteras y Cuotéalo
+											xculqirsaid: 'Inserta aquí el id de tu llave pública RSA',
+											rsapublickey: 'Inserta aquí tu llave pública RSA',
+										});
+
+										Culqi.options({
+											lang: "auto",
+											installments: false, // Habilitar o deshabilitar el campo de cuotas
+											paymentMethods: {
+											tarjeta: true,
+											yape: true,
+											bancaMovil: true,
+											agente: true,
+											billetera: true,
+											cuotealo: true,
+											},
+										});
+
+									Culqi.options({
+										style: {
+											logo: 'https://culqi.com/LogoCulqi.png',
+											bannerColor: '', // hexadecimal
+											buttonBackground: '', // hexadecimal
+											menuColor: '', // hexadecimal
+											linksColor: '', // hexadecimal
+											buttonText: '', // texto que tomará el botón
+											buttonTextColor: '', // hexadecimal
+											priceColor: '' // hexadecimal
+										}
+									});
+
+
+									
+
+
+
+									Culqi.open();
+									e.preventDefault();
+								})
+
+								function culqi() {
+										if (Culqi.token) {  // ¡Objeto Token creado exitosamente!
+										const token = Culqi.token.id;
+										console.log('Se ha creado un Token: ', token);
+										//En esta linea de codigo debemos enviar el "Culqi.token.id"
+										//hacia tu servidor con Ajax
+										} else if (Culqi.order) {  // ¡Objeto Order creado exitosamente!
+										const order = Culqi.order;
+										console.log('Se ha creado el objeto Order: ', order);
+
+										} else {
+										// Mostramos JSON de objeto error en consola
+										console.log('Error : ',Culqi.error);
+										}
+									};
+
+								</script>
+<p>adios culqui</p>
+
+<!-- culquiiiiiiiiiiiii -->
 								<div>
 									<label for="contraentrega">
 										<input type="radio" id="contraentrega" class="methodpago" name="payment-method" value="CT">
