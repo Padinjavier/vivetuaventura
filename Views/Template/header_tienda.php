72d0331b21d4b86aca08f731113e199fd79a6e12
Javier <javierpadin661@gmail.com>
2023-12-22 23:25:58 -0500

    active de los apartados del header tienda arreglado

--- Views/Template/header_tienda.php
+++ Views/Template/header_tienda.php
@@ -1,101 +1,109 @@
-<?php 
-	$cantCarrito = 0;
-	if(isset($_SESSION['arrCarrito']) and count($_SESSION['arrCarrito']) > 0){ 
-		foreach($_SESSION['arrCarrito'] as $product) {
-			$cantCarrito += $product['cantidad'];
-		}
+<?php
+$cantCarrito = 0;
+if (isset($_SESSION['arrCarrito']) and count($_SESSION['arrCarrito']) > 0) {
+	foreach ($_SESSION['arrCarrito'] as $product) {
+		$cantCarrito += $product['cantidad'];
 	}
-	$tituloPreguntas = !empty(getInfoPage(PPREGUNTAS)) ? getInfoPage(PPREGUNTAS)['titulo'] : "";
-	$infoPreguntas = !empty(getInfoPage(PPREGUNTAS)) ? getInfoPage(PPREGUNTAS)['contenido'] : "";
- ?>
+}
+$tituloPreguntas = !empty(getInfoPage(PPREGUNTAS)) ? getInfoPage(PPREGUNTAS)['titulo'] : "";
+$infoPreguntas = !empty(getInfoPage(PPREGUNTAS)) ? getInfoPage(PPREGUNTAS)['contenido'] : "";
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-	<title><?= $data['page_tag']; ?></title>
+	<title>
+		<?= $data['page_tag']; ?>
+	</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-	<?php 
-		$nombreSitio = NOMBRE_EMPESA;
+	<?php
+	$nombreSitio = NOMBRE_EMPESA;
+	$descripcion = DESCRIPCION;
+	$nombreProducto = NOMBRE_EMPESA;
+	$urlWeb = base_url();
+	$urlImg = media() . "/images/portada.jpg";
+	if (!empty($data['producto'])) {
+		//$descripcion = $data['producto']['descripcion'];
 		$descripcion = DESCRIPCION;
-		$nombreProducto = NOMBRE_EMPESA;
-		$urlWeb = base_url();
-		$urlImg = media()."/images/portada.jpg";
-		if(!empty($data['producto'])){
-			//$descripcion = $data['producto']['descripcion'];
-			$descripcion = DESCRIPCION;
-			$nombreProducto = $data['producto']['nombre'];
-			$urlWeb = base_url()."/tienda/producto/".$data['producto']['idproducto']."/".$data['producto']['ruta'];
-			$urlImg = $data['producto']['images'][0]['url_image'];
-		}
+		$nombreProducto = $data['producto']['nombre'];
+		$urlWeb = base_url() . "/tienda/producto/" . $data['producto']['idproducto'] . "/" . $data['producto']['ruta'];
+		$urlImg = $data['producto']['images'][0]['url_image'];
+	}
 	?>
-	<meta property="og:locale" 		content='es_ES'/>
-	<meta property="og:type"        content="website" />
-	<meta property="og:site_name"	content="<?= $nombreSitio; ?>"/>
+	<meta property="og:locale" content='es_ES' />
+	<meta property="og:type" content="website" />
+	<meta property="og:site_name" content="<?= $nombreSitio; ?>" />
 	<meta property="og:description" content="<?= $descripcion; ?>" />
-	<meta property="og:title"       content="<?= $nombreProducto; ?>" />
-	<meta property="og:url"         content="<?= $urlWeb; ?>" />
-	<meta property="og:image"       content="<?= $urlImg; ?>" />
+	<meta property="og:title" content="<?= $nombreProducto; ?>" />
+	<meta property="og:url" content="<?= $urlWeb; ?>" />
+	<meta property="og:image" content="<?= $urlImg; ?>" />
 
-<!--===============================================================================================-->	
-	<link rel="icon" type="image/png" href="<?= media() ?>/tienda/images/favicon.ico"/>
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
+	<link rel="icon" type="image/png" href="<?= media() ?>/tienda/images/favicon.ico" />
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="<?= media() ?>/tienda/vendor/bootstrap/css/bootstrap.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?= media() ?>/tienda/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="<?= media() ?>/tienda/fonts/iconic/css/material-design-iconic-font.min.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css"
+		href="<?= media() ?>/tienda/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+	<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css"
+		href="<?= media() ?>/tienda/fonts/iconic/css/material-design-iconic-font.min.css">
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="<?= media() ?>/tienda/fonts/linearicons-v1.0.0/icon-font.min.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="<?= media() ?>/tienda/vendor/animate/animate.css">
-<!--===============================================================================================-->	
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="<?= media() ?>/tienda/vendor/css-hamburgers/hamburgers.min.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="<?= media() ?>/tienda/vendor/animsition/css/animsition.min.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="<?= media() ?>/tienda/vendor/select2/select2.min.css">
-<!--===============================================================================================-->	
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="<?= media() ?>/tienda/vendor/daterangepicker/daterangepicker.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="<?= media() ?>/tienda/vendor/slick/slick.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="<?= media() ?>/tienda/vendor/MagnificPopup/magnific-popup.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="<?= media() ?>/tienda/vendor/perfect-scrollbar/perfect-scrollbar.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 	<link rel="stylesheet" type="text/css" href="<?= media() ?>/tienda/css/util.css">
 	<link rel="stylesheet" type="text/css" href="<?= media() ?>/tienda/css/main.css">
 	<link rel="stylesheet" type="text/css" href="<?= media(); ?>/css/style.css">
-<!--===============================================================================================-->
+	<!--===============================================================================================-->
 </head>
+
 <body class="animsition">
 	<!-- Modal -->
 	<div class="modal fade" id="modalAyuda" tabindex="-1" aria-hidden="true">
-	  <div class="modal-dialog modal-lg">
-	    <div class="modal-content">
-	      <div class="modal-header">
-	        <h5 class="modal-title"><?= $tituloPreguntas ?></h5>
-	        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-	          <span aria-hidden="true">&times;</span>
-	        </button>
-	      </div>
-	      <div class="modal-body">
-	      		<div class="page-content">
-	        		<?= $infoPreguntas; ?>
-	      		</div>
-	      </div>
-	      <div class="modal-footer">
-	        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
-	      </div>
-	    </div>
-	  </div>
+		<div class="modal-dialog modal-lg">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title">
+						<?= $tituloPreguntas ?>
+					</h5>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<div class="modal-body">
+					<div class="page-content">
+						<?= $infoPreguntas; ?>
+					</div>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+				</div>
+			</div>
+		</div>
 	</div>
-	<div id="divLoading" >
-      <div>
-        <img src="<?= media(); ?>/images/loading.svg" alt="Loading">
-      </div>
-    </div>
+	<div id="divLoading">
+		<div>
+			<img src="<?= media(); ?>/images/loading.svg" alt="Loading">
+		</div>
+	</div>
 	<!-- Header -->
 	<header>
 		<!-- Header desktop -->
@@ -104,31 +112,32 @@
 			<div class="top-bar">
 				<div class="content-topbar flex-sb-m h-full container">
 					<div class="left-top-bar">
-						<?php if(isset($_SESSION['login'])){ ?>
-						Bienvenido: <?= $_SESSION['userData']['nombres'].' '.$_SESSION['userData']['apellidos'] ?>
+						<?php if (isset($_SESSION['login'])) { ?>
+							Bienvenido:
+							<?= $_SESSION['userData']['nombres'] . ' ' . $_SESSION['userData']['apellidos'] ?>
 						<?php } ?>
 					</div>
 
 					<div class="right-top-bar flex-w h-full">
-						<a href="#" class="flex-c-m trans-04 p-lr-25" data-toggle="modal" data-target="#modalAyuda" >
+						<a href="#" class="flex-c-m trans-04 p-lr-25" data-toggle="modal" data-target="#modalAyuda">
 							Help & FAQs
 						</a>
-						<?php 
-							if(isset($_SESSION['login'])){
-						?>
-						<a href="<?= base_url() ?>/dashboard" class="flex-c-m trans-04 p-lr-25">
-							Mi cuenta
-						</a>
-						<?php } 
-							if(isset($_SESSION['login'])){
-						?>
-						<a href="<?= base_url() ?>/logout" class="flex-c-m trans-04 p-lr-25">
-							Salir
-						</a>
-						<?php }else{ ?>
-						<a href="<?= base_url() ?>/login" class="flex-c-m trans-04 p-lr-25">
-							Iniciar Sesión
-						</a>
+						<?php
+						if (isset($_SESSION['login'])) {
+							?>
+							<a href="<?= base_url() ?>/dashboard" class="flex-c-m trans-04 p-lr-25">
+								Mi cuenta
+							</a>
+						<?php }
+						if (isset($_SESSION['login'])) {
+							?>
+							<a href="<?= base_url() ?>/logout" class="flex-c-m trans-04 p-lr-25">
+								Salir
+							</a>
+						<?php } else { ?>
+							<a href="<?= base_url() ?>/login" class="flex-c-m trans-04 p-lr-25">
+								Iniciar Sesión
+							</a>
 						<?php } ?>
 					</div>
 				</div>
@@ -136,8 +145,8 @@
 
 			<div class="wrap-menu-desktop">
 				<nav class="limiter-menu-desktop container">
-					
-					<!-- Logo desktop -->		
+
+					<!-- Logo desktop -->
 					<a href="<?= base_url(); ?>" class="logo">
 						<img src="<?= media() ?>/tienda/images/logoChamorro.png" alt="Tienda Virtual">
 					</a>
@@ -156,7 +165,7 @@
 							<li>
 								<a href="<?= base_url(); ?>/carrito">Carrito</a>
 							</li>
-							
+
 							<li>
 								<a href="<?= base_url(); ?>/nosotros">Nosotros</a>
 							</li>
@@ -169,28 +178,30 @@
 								<a href="<?= base_url(); ?>/contacto">Contacto</a>
 							</li>
 						</ul>
-					</div>	
+					</div>
 
 					<!-- Icon header -->
 					<div class="wrap-icon-header flex-w flex-r-m">
 						<div class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 js-show-modal-search">
 							<i class="zmdi zmdi-search"></i>
 						</div>
-						<?php if($data['page_name'] != "carrito" and $data['page_name'] != "procesarpago"){ ?>
-						<div class="cantCarrito icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 icon-header-noti js-show-cart" data-notify="<?= $cantCarrito; ?> ">
-							<i class="zmdi zmdi-shopping-cart"></i>
-						</div>
+						<?php if ($data['page_name'] != "carrito" and $data['page_name'] != "procesarpago") { ?>
+							<div class="cantCarrito icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 icon-header-noti js-show-cart"
+								data-notify="<?= $cantCarrito; ?> ">
+								<i class="zmdi zmdi-shopping-cart"></i>
+							</div>
 						<?php } ?>
 					</div>
 				</nav>
-			</div>	
+			</div>
 		</div>
 
 		<!-- Header Mobile -->
 		<div class="wrap-header-mobile">
-			<!-- Logo moblie -->		
+			<!-- Logo moblie -->
 			<div class="logo-mobile">
-				<a href="<?= base_url(); ?>"><img src="<?= media() ?>/tienda/images/logoChamorro.png" alt="Tienda Virtual"></a>
+				<a href="<?= base_url(); ?>"><img src="<?= media() ?>/tienda/images/logoChamorro.png"
+						alt="Tienda Virtual"></a>
 			</div>
 
 			<!-- Icon header -->
@@ -198,10 +209,11 @@
 				<div class="icon-header-item cl2 hov-cl1 trans-04 p-r-11 js-show-modal-search">
 					<i class="zmdi zmdi-search"></i>
 				</div>
-				<?php if($data['page_name'] != "carrito" and $data['page_name'] != "procesarpago"){ ?>
-				<div class="cantCarrito icon-header-item cl2 hov-cl1 trans-04 p-r-11 p-l-10 icon-header-noti js-show-cart" data-notify="<?= $cantCarrito; ?>">
-					<i class="zmdi zmdi-shopping-cart"></i>
-				</div>
+				<?php if ($data['page_name'] != "carrito" and $data['page_name'] != "procesarpago") { ?>
+					<div class="cantCarrito icon-header-item cl2 hov-cl1 trans-04 p-r-11 p-l-10 icon-header-noti js-show-cart"
+						data-notify="<?= $cantCarrito; ?>">
+						<i class="zmdi zmdi-shopping-cart"></i>
+					</div>
 				<?php } ?>
 			</div>
 
@@ -219,8 +231,9 @@
 			<ul class="topbar-mobile">
 				<li>
 					<div class="left-top-bar">
-						<?php if(isset($_SESSION['login'])){ ?>
-						Bienvenido: <?= $_SESSION['userData']['nombres'].' '.$_SESSION['userData']['apellidos'] ?>
+						<?php if (isset($_SESSION['login'])) { ?>
+							Bienvenido:
+							<?= $_SESSION['userData']['nombres'] . ' ' . $_SESSION['userData']['apellidos'] ?>
 						<?php } ?>
 					</div>
 				</li>
@@ -230,22 +243,22 @@
 						<a href="#" class="flex-c-m p-lr-10 trans-04" data-toggle="modal" data-target="#modalAyuda">
 							Help & FAQs
 						</a>
-						<?php 
-							if(isset($_SESSION['login'])){
-						?>
-						<a href="<?= base_url() ?>/dashboard" class="flex-c-m trans-04 p-lr-25">
-							Mi cuenta
-						</a>
-						<?php } 
-							if(isset($_SESSION['login'])){
-						?>
-						<a href="<?= base_url() ?>/logout" class="flex-c-m trans-04 p-lr-25">
-							Salir
-						</a>
-						<?php }else{ ?>
-						<a href="<?= base_url() ?>/login" class="flex-c-m trans-04 p-lr-25">
-							Iniciar Sesión
-						</a>
+						<?php
+						if (isset($_SESSION['login'])) {
+							?>
+							<a href="<?= base_url() ?>/dashboard" class="flex-c-m trans-04 p-lr-25">
+								Mi cuenta
+							</a>
+						<?php }
+						if (isset($_SESSION['login'])) {
+							?>
+							<a href="<?= base_url() ?>/logout" class="flex-c-m trans-04 p-lr-25">
+								Salir
+							</a>
+						<?php } else { ?>
+							<a href="<?= base_url() ?>/login" class="flex-c-m trans-04 p-lr-25">
+								Iniciar Sesión
+							</a>
 						<?php } ?>
 					</div>
 				</li>
@@ -285,7 +298,7 @@
 					<img src="<?= media() ?>/tienda/images/icons/icon-close2.png" alt="CLOSE">
 				</button>
 
-				<form class="wrap-search-header flex-w p-l-15" method="get" action="<?= base_url() ?>/tienda/search" >
+				<form class="wrap-search-header flex-w p-l-15" method="get" action="<?= base_url() ?>/tienda/search">
 					<button class="flex-c-m trans-04">
 						<i class="zmdi zmdi-search"></i>
 					</button>
@@ -309,7 +322,7 @@
 				</div>
 			</div>
 			<div id="productosCarrito" class="header-cart-content flex-w js-pscroll">
-				<?php getModal('modalCarrito',$data); ?>
+				<?php getModal('modalCarrito', $data); ?>
 			</div>
 		</div>
 	</div>
