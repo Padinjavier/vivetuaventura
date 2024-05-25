90f43dcf9180777363344c5563d4df756bf08acf
pctrabajo <javierpadin661@gmail.com>
2024-05-25 18:05:43 -0500

    db

--- agencia4.sql
+++ agencia4.sql
@@ -119,10 +119,12 @@ CREATE TABLE IF NOT EXISTS `opciones` (
   PRIMARY KEY (`id`) USING BTREE,
   KEY `idpersona` (`personaid`) USING BTREE,
   CONSTRAINT `FK_opciones_persona` FOREIGN KEY (`personaid`) REFERENCES `persona` (`idpersona`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
 -- Volcando datos para la tabla agencia.opciones: ~0 rows (aproximadamente)
 DELETE FROM `opciones`;
+INSERT INTO `opciones` (`id`, `personaid`, `idioma`, `tema`, `formato_moneda`) VALUES
+	(13, 22, 1, 3, 1);
 
 -- Volcando estructura para tabla agencia.pedido
 CREATE TABLE IF NOT EXISTS `pedido` (
@@ -245,17 +247,21 @@ CREATE TABLE IF NOT EXISTS `persona` (
   KEY `rolidempleado` (`rolidempleado`),
   CONSTRAINT `FK_persona_rol` FOREIGN KEY (`rolid`) REFERENCES `rol_usuario` (`idrolusuario`),
   CONSTRAINT `FK_persona_rol_empleado` FOREIGN KEY (`rolidempleado`) REFERENCES `rol_empleado` (`idrolempleado`)
-) ENGINE=InnoDB AUTO_INCREMENT=56 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=60 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_swedish_ci;
 
 -- Volcando datos para la tabla agencia.persona: ~6 rows (aproximadamente)
 DELETE FROM `persona`;
 INSERT INTO `persona` (`idpersona`, `identificacion`, `nombres`, `apellidos`, `telefono`, `email_user`, `password`, `nit`, `nombrefiscal`, `direccionfiscal`, `token`, `rolid`, `datecreated`, `status`, `direccion`, `ciudad`, `rolidempleado`) VALUES
 	(2, '73621360', 'Jeanettis Mariel', 'Luyo Correa', 910089718, 'jluyo@gmail.com', '5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5', NULL, NULL, NULL, NULL, 2, '2023-11-29 10:57:56', 1, NULL, NULL, NULL),
 	(22, '74199531', 'Javier Antonio', 'Padin Flores', 917189300, 'javierpadin661@gmail.com', 'afad7b36d11a0e2c7b30ec3a16c9077d8e2c4117f282f257790bd9f70641d840', 'ID tributo javier', 'NAME tributo javier', 'ADDRES tributo javier', '0b29bd63a450601e8de9-46a10d5e89d9c8d8010f-34f01ff7f4adc772e209-2598a10910d6573c55cd', 1, '2023-11-10 03:11:09', 1, '111111', '2222', NULL),
-	(24, 'JUAN LLOCYA', 'Juan Manuel', 'Llocya Castro', 918313532, 'jllocya@system32.shop', '', 'sssssss', 'sssss', 'sssssssssss', NULL, NULL, '2023-11-29 11:03:25', 1, NULL, NULL, 1),
+	(24, 'JUAN LLOCYA', 'Juan Manuel', 'Llocya Castro', 918313532, 'jllocya@system32.shop', '', 'sssssss', 'sssss', 'sssssssssss', NULL, NULL, '2023-11-29 11:03:25', 0, NULL, NULL, 1),
 	(28, '72014145', 'Alex', 'Huasasquiche', 946454569, 'ahuasasquiche@gmail.com', '', NULL, NULL, NULL, NULL, NULL, '2023-12-06 17:27:31', 1, NULL, NULL, 1),
 	(29, '74199532', 'Javier Padin UNDC', 'Padin Flores', 9171893004, '2002010167@undc.edu.pe', '', NULL, NULL, '1111', NULL, NULL, '2023-12-16 10:41:03', 1, 'Av Lima', 'Quilmana', 1),
-	(30, '4444444444', 'FRANK', 'GALAGER', 99999999, 'FRANK@gmail.com', 'afad7b36d11a0e2c7b30ec3a16c9077d8e2c4117f282f257790bd9f70641d840', NULL, NULL, NULL, NULL, 3, '2024-05-12 23:57:54', 1, NULL, NULL, NULL);
+	(30, '4444444444', 'FRANK', 'GALAGER', 99999999, 'FRANK@gmail.com', 'afad7b36d11a0e2c7b30ec3a16c9077d8e2c4117f282f257790bd9f70641d840', NULL, NULL, NULL, NULL, 3, '2024-05-12 23:57:54', 1, NULL, NULL, NULL),
+	(56, '75486524', 'JHON VIGNEY', 'ROMERO MELCHOR', 918313532, 'javierpadin6w61@gmail.com', NULL, NULL, NULL, NULL, NULL, NULL, '2024-05-25 08:34:47', 1, NULL, NULL, 20),
+	(57, '23232323', 'Jeanettis Mariel', 'CHAVEZ SANCHEZ', 910089718, 'javierpad23in661@gmail.com', NULL, NULL, NULL, NULL, NULL, NULL, '2024-05-25 08:39:53', 1, NULL, NULL, 21),
+	(58, '2323234', 'Juangg', 'Herrera', 111111111111111, 'javiegggrpadin661@gmail.com', NULL, NULL, NULL, NULL, NULL, NULL, '2024-05-25 08:40:10', 1, NULL, NULL, 22),
+	(59, '666', 'NANCY YANINA', 'MAZA SUAREZ', 910089718, 'javierpa66din661@gmail.com', NULL, NULL, NULL, NULL, NULL, NULL, '2024-05-25 09:25:41', 0, NULL, NULL, 23);
 
 -- Volcando estructura para tabla agencia.post
 CREATE TABLE IF NOT EXISTS `post` (
@@ -327,12 +333,16 @@ CREATE TABLE IF NOT EXISTS `rol_empleado` (
   `descripcion` text CHARACTER SET utf8mb4 COLLATE utf8mb4_swedish_ci NOT NULL,
   `status` int NOT NULL DEFAULT '1',
   PRIMARY KEY (`idrolempleado`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_swedish_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_swedish_ci;
 
 -- Volcando datos para la tabla agencia.rol_empleado: ~1 rows (aproximadamente)
 DELETE FROM `rol_empleado`;
 INSERT INTO `rol_empleado` (`idrolempleado`, `nombrerolempleado`, `descripcion`, `status`) VALUES
-	(1, 'Guia', 'GUIAR A LOS TURISTAS', 1);
+	(1, 'Guia', 'GUIAR A LOS TURISTAS', 1),
+	(20, 'CONDUCTIR', '111', 1),
+	(21, 'KAYACK', '222', 1),
+	(22, 'CARGADOR', '333', 1),
+	(23, 'cargador', 'vvv', 0);
 
 -- Volcando estructura para tabla agencia.rol_usuario
 CREATE TABLE IF NOT EXISTS `rol_usuario` (
