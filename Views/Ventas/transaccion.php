b592029852b792cfc791fe087bcbf7bea0331419
xavi661 <javierpadin661@gmail.com>
2024-06-17 08:41:42 -0500

    b

--- Views/Ventas/transaccion.php
+++ Views/Ventas/transaccion.php
@@ -7,7 +7,7 @@
     </div>
     <ul class="app-breadcrumb breadcrumb">
       <li class="breadcrumb-item"><i class="fa fa-home fa-lg"></i></li>
-      <li class="breadcrumb-item"><a href="<?= base_url(); ?>/pedidos"> Ventas</a></li>
+      <li class="breadcrumb-item"><a href="<?= base_url(); ?>/ventas"> Ventas</a></li>
     </ul>
   </div>
   <div class="row">
